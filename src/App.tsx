5b4959651b562af11e119d07f13e55459c8ccfb9
paul <paul.briere94@gmail.com>
2023-01-05 13:49:46 -0500

    inital commit

---
+++ src/App.tsx
@@ -0,0 +1,87 @@
+import ResponsiveAppBar from "./components/ResponsiveAppBar"
+import { Page } from "./components/Page"
+import { ThemeProvider } from "@mui/material"
+import { theme } from './theme'
+import { Container, Divider, Box } from "@mui/material"
+import { Home } from "./pages/Home"
+import { Music } from "./pages/Music"
+import { About } from "./pages/About"
+import { Contact } from "./pages/Contact"
+import { useLocation } from "react-router-dom"
+import { useEffect, useState } from "react"
+import LoadingPage from "./components/LoadingPage"
+
+export const APP_BAR_HEIGHT = 80
+
+function App() {
+
+  const [loading, setLoading] = useState(true);
+
+  const location = useLocation();
+
+  useEffect(() => {
+    // External redirection
+    switch (location.pathname) {
+      case '/portfolio':
+        window.location.replace('https://muelmuel.notion.site/Paul-Bri-re-P-Side-4963602657f94fa383ee73cb0d6877ce')
+        break
+      case '/epk':
+        window.location.replace('https://muelmuel.notion.site/P-Side-151795fcfe004ff4bd392319625e5214')
+        break
+    }
+
+    if (loading) {
+      return
+    }
+    scrollToLocation()
+  }, [location])
+
+  const imagesLoaded = () => {
+    setLoading(false)
+    scrollToLocation()
+  }
+  function scrollToLocation() {
+    let id = location.pathname.split('/').at(-1)
+    if (!id) {
+      id = 'home'
+    }
+    var elementPosition = document.getElementById(id)?.getBoundingClientRect().top
+    var offsetPosition = (elementPosition ? elementPosition : 0) + window.pageYOffset - APP_BAR_HEIGHT
+
+    window.scrollTo({
+      top: offsetPosition,
+      behavior: "smooth"
+    });
+  };
+
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <LoadingPage open={loading} />
+        <Home id='home' />
+        <ResponsiveAppBar />
+        <Box
+          sx={{
+            backgroundImage: `linear-gradient(0deg, rgb(255 244 216) 0%, ${theme.palette.primary.main} 100%)`,
+            backgroundRepeat: 'no-repeat',
+          }}>
+          <Container id='body'>
+            <Page title="Music" id="music">
+              <Music onAllImagesLoaded={() => imagesLoaded()} />
+            </Page>
+            <Divider />
+            <Page title="About" id="about">
+              <About />
+            </Page>
+            <Divider />
+            <Page title="Contact" id="contact">
+              <Contact />
+            </Page>
+          </Container>
+        </Box>
+      </ThemeProvider>
+    </>
+  )
+}
+
+export default App
